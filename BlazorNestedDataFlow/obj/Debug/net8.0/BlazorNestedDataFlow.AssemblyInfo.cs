9dcb480cf5f60485a3c4971c6deb6fc46f5d8acd
AlienWashim <ws.akram330@gmail.com>
2024-12-18 10:15:08 +0600

    source code

--- BlazorNestedDataFlow/obj/Debug/net8.0/BlazorNestedDataFlow.AssemblyInfo.cs
+++ BlazorNestedDataFlow/obj/Debug/net8.0/BlazorNestedDataFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorNestedDataFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68111b7d97372da20bef1ce0fa27c042fb9fc0c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorNestedDataFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorNestedDataFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
